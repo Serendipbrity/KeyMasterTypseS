bbe5cb558056b752223d18cb3962148aedcbd28e
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2024-05-12 16:47:42 -0400

    fixed typo

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/KeyMasterTypeS/',
+  base: '/KeyMasterTypseS/',
   plugins: [react()],
   build: {
     outDir: 'dist',  // Ensures that the build output goes to the 'dist' directory
   },
   server: {
-    port: 3000,
     open: true
   }
 })
