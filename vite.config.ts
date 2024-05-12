281c731c386fc6b5b0b68ae64d4230019bb10c12
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2024-05-12 16:25:49 -0400

    added base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/KeyMasterTypeS/',
   plugins: [react()],
   build: {
     outDir: 'dist',  // Ensures that the build output goes to the 'dist' directory
